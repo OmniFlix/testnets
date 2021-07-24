13a6c82c0b51a09a00194b8d4f2875d6388b514c
harish <harishmarri551@gmail.com>
2021-07-24 18:10:01 +0530

    Fix2

--- scripts/verify_gentx.sh
+++ scripts/verify_gentx.sh
@@ -6,8 +6,8 @@ CHAIN_ID=flixnet-1
 GENTX_FILE=$(find ./$CHAIN_ID/gentxs -iname "*.json")
 LEN_GENTX=$(echo ${#GENTX_FILE})
 
-GENTX_DEADLINE=$(date -d '26-07-2021 18:00:00' '+%d/%m/%Y %H:%M:%S')
-now=$(date +"%d/%m/%Y %H:%M:%S")
+GENTX_DEADLINE=$(date -u -d '2021-07-26T18:00:00.000Z' +'%s')
+now=$(date -u +'%s')
 
 declare -i maxbond=50000000
 
@@ -36,48 +36,45 @@ else
         echo "bonded amount is too high: $amt > $maxbond"
         exit 1
     fi
-
     echo "...........Init omniflixhub.............."
 
-    git clone https://github.com/OmniFlix/omniflixhub
-    cd omniflixhub
-    git checkout v0.1.0
-    make install
+    wget -q https://github.com/OmniFlix/omniflixhub/releases/download/v0.1.0/omniflixhubd -O omniflixhubd
+    chmod +x omniflixhubd
+    
+    ./omniflixhubd keys add $RANDOM_KEY --home $FLIX_HOME
 
-    omniflixhub keys add $RANDOM_KEY --home $FLIX_HOME
-
-    omniflixhub init --chain-id $CHAIN_ID validator --home $FLIX_HOME
+    ./omniflixhubd init --chain-id $CHAIN_ID validator --home $FLIX_HOME
 
     echo "..........Updating genesis......."
     sed -i "s/\"stake\"/\"uflix\"/g" $FLIX_HOME/config/genesis.json
 
-    GENACC=$(cat ../$GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p')
+    GENACC=$(jq -r '.body.messages[0].delegator_address' $GENTX_FILE)
 
     echo $GENACC
 
-    omniflixhub add-genesis-account $RANDOM_KEY 50000000uflix --home $FLIX_HOME --keyring-backend test
-    omniflixhub add-genesis-account $GENACC 50000000uflix --home $FLIX_HOME
+    ./omniflixhubd add-genesis-account $RANDOM_KEY 50000000uflix --home $FLIX_HOME --keyring-backend test
+    ./omniflixhubd add-genesis-account $GENACC 50000000uflix --home $FLIX_HOME
 
-    omniflixhub gentx $RANDOM_KEY 40000000uflix --home $FLIX_HOME \
+    ./omniflixhubd gentx $RANDOM_KEY 40000000uflix --home $FLIX_HOME \
          --keyring-backend test --chain-id $CHAIN_ID
-    cp ../$GENTX_FILE $FLIX_HOME/config/gentx/
+    cp $GENTX_FILE $FLIX_HOME/config/gentx/
 
     echo "..........Collecting gentxs......."
-    omniflixhub collect-gentxs --home $FLIX_HOME
+    ./omniflixhubd collect-gentxs --home $FLIX_HOME
     sed -i '/persistent_peers =/c\persistent_peers = ""' $FLIX_HOME/config/config.toml
 
-    omniflixhub validate-genesis --home $FLIX_HOME
+    ./omniflixhubd validate-genesis --home $FLIX_HOME
 
     echo "..........Starting node......."
-    omniflixhub start --home $FLIX_HOME &
+    ./omniflixhubd start --home $FLIX_HOME &
 
     sleep 5s
 
     echo "...checking network status.."
 
-    omniflixhubd status --node http://localhost:26657
+    ./omniflixhubd status --node http://localhost:26657
 
     echo "...Cleaning ..."
-    killall omniflixhub >/dev/null 2>&1
+    killall omniflixhubd >/dev/null 2>&1
     rm -rf $FLIX_HOME >/dev/null 2>&1
 fi
